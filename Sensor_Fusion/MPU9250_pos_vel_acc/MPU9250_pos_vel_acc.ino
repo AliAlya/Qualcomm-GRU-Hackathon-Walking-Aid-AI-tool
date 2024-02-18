eb93a4a72d801cdb2264363a36c69377e7b0937a
Arwin Sondhi <arwin.sondhi@mail.utoronto.ca>
2024-02-17 20:42:38 -0500

    reading works for both sensors on pc

--- Sensor_Fusion/MPU9250_pos_vel_acc/MPU9250_pos_vel_acc.ino
+++ Sensor_Fusion/MPU9250_pos_vel_acc/MPU9250_pos_vel_acc.ino
@@ -37,6 +37,7 @@ void loop() {
 
   result = mySensor.readId(&sensorId);
 
+    // PRINT POS FIRST
     if (mpu.update()) {
       
         // Serial.println(
@@ -48,15 +49,16 @@ void loop() {
     }
 
 
+  // PRINT VEL
   result = mySensor.gyroUpdate();
   if (result == 0) {
     gX = mySensor.gyroX();
     gY = mySensor.gyroY();
     gZ = mySensor.gyroZ();
 
-    Serial.println(
-      "Vel: "
-    );
+    // Serial.println(
+    //   "Vel: "
+    // );
     Serial.print(gX); Serial.print(",");
         Serial.print(gY); Serial.print(",");
         Serial.print(gZ);  Serial.print(",");
@@ -67,7 +69,8 @@ void loop() {
     // Serial.println("Cannot read gyro values " + String(result));
   }
 
-    result = mySensor.accelUpdate();
+  // PRINT ACCEL
+  result = mySensor.accelUpdate();
   if (result == 0) {
     aX = mySensor.accelX();
     aY = mySensor.accelY();
@@ -78,11 +81,11 @@ void loop() {
     // Serial.println("accelZ: " + String(aZ));
     // Serial.println("accelSqrt: " + String(aSqrt));
 
-    Serial.println(
-      "Acc: "
-    );
-    Serial.print(aX); Serial.print(", ");
-        Serial.print(aY); Serial.print(", ");
+    // Serial.println(
+    //   "Acc: "
+    // );
+    Serial.print(aX); Serial.print(",");
+        Serial.print(aY); Serial.print(",");
         Serial.println(aZ); //Serial.print(", ");
   } else {
     // Serial.println("Cannod read accel values " + String(result));
--- Sensor_Fusion/pi/read.py
+++ Sensor_Fusion/pi/read.py
@@ -3,15 +3,22 @@ import csv
 from datetime import datetime
 from time import time
 
-# Open serial port
-ser = serial.Serial('COM4', 9600) # Adjust 'COM3' to your Arduino's serial port
+# Open serial ports
+ser1 = serial.Serial('COM4', 9600)  # Adjust 'COM4' to your device's serial port
+ser2 = serial.Serial('COM3', 115200)  # Adjust 'COM3' to your device's serial port
 
 # Open or create a CSV file
-with open('obs_' + datetime.now().strftime("%d %H_%M_%S") + '.csv', 'w', newline='') as file:
+with open('obs_' + datetime.now().strftime("%d_%H_%M_%S") + '.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     start = time()
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip() # Read a line from the serial port
-            data = [time() - start] + line.split(',') # Prepend timestamp
-            writer.writerow(data) # Write data to CSV
+        # data = [time() - start]  # Initialize data list with elapsed time
+        if ser1.in_waiting > 0 and ser2.in_waiting > 0: # both buffers have data
+            line1 = ser1.readline().decode('utf-8').rstrip()  # Read a line from COM4
+            data = [time() - start] + line1.split(',') # Prepend timestamp
+
+            line2 = ser2.readline().decode('utf-8').rstrip()  # Read a line from COM3
+            data += line2.split(',')  # Append data from COM3
+
+            writer.writerow(data)  # Write combined data to CSV
+
