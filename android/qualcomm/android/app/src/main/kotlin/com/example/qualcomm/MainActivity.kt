109e202923e406ac795dedcedfe5d2f98c138730
Arwin Sondhi <arwin.sondhi@mail.utoronto.ca>
2024-02-18 10:57:07 -0500

    flutter usb comm to qualcomm kit

---
+++ android/qualcomm/android/app/src/main/kotlin/com/example/qualcomm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qualcomm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/usb_comm/android/app/src/main/kotlin/com/example/usb_comm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.usb_comm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
